324e1945d9f1141db777facdc0216809e3f1a497
vabs <njoy.today@gmail.com>
2020-08-04 21:32:01 -0400

    Updates

---
+++ precache-manifest.637276c10d75cf3b99e40691acaf6941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29e4817687dae097bf3f0176216086f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a624ec68bb43b800cdbf",
+    "url": "/static/css/main.bc3002e3.chunk.css"
+  },
+  {
+    "revision": "09bd5894df1103efeb77",
+    "url": "/static/js/2.cdcd4914.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.cdcd4914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a624ec68bb43b800cdbf",
+    "url": "/static/js/main.7c70b8b0.chunk.js"
+  },
+  {
+    "revision": "8dff55f1ca3c7cb811e0",
+    "url": "/static/js/runtime-main.f75d8cfa.js"
+  }
+]);
